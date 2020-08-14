38e441bf366b5baacb2a7ea05851ddf11f7fc2f8
Kelin Field <kfield111@hotmail.com>
2020-08-14 00:55:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,9 @@
 class Song
   attr_reader :name, :artist, :genre
 
-def initialize (name, artist)
+def initialize (name, artist, genre)
   @name = name
   @artist = artist
-end
-
-def genre=(genre)
-  @genre = genre
 end
 
 
