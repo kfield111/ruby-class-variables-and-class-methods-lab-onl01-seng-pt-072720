288767de6970d6e5a3693956a1410eb9a6a2d626
Kelin Field <kfield111@hotmail.com>
2020-08-14 02:28:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,7 @@ class Song
 @@count = 0
 @@artists = []
 @@genres = []
+@@genre_count = {}
 
 def initialize (name, artist, genre)
   @name = name
@@ -28,17 +29,31 @@ def self.genres
   @@genres.uniq
 end
 
-def self.genre_count
-  hash = {}
-  @@genres.each do |this_genre|
-    if hash.include?(this_genre)
-      song_count += 1
-      binding.pry
-    else
-      hash[this_genre] = song_count
-    end
+
+def self.genre_count(song_count, genre)
+  @roster[grade] ||= []
+  @roster[grade].push (name)
   end
-  hash
 end
 
+# def self.genre_count
+#   hash = {}
+#   @@genres.each do |this_genre|
+#     if hash.include?(this_genre)
+#       song_count += 1
+#       binding.pry
+#     else
+#       hash[this_genre] = song_count
+#     end
+#   end
+#   hash
+# end
+
+end
+
+
+def add_student(name, grade)
+  @roster[grade] ||= []
+  @roster[grade].push (name)
+  end
 end
