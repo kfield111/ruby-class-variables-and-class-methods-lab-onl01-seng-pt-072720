8d61f0df6dd7fe6e6af2c624e96826cedac040df
Kelin Field <kfield111@hotmail.com>
2020-08-14 02:35:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,13 +32,13 @@ end
 
 def self.genre_count
   hash = {}
-  song_count = 0
+  song_count = 1
   @@genres.each do |this_genre|
     if hash.include?(this_genre)
       song_count + 1
       #binding.pry
     else
-      hash[this_genre] = song_count += 1
+      hash[this_genre] = song_count
     end
   end
   hash
